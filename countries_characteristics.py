10fc2dbc05e472ebaf8203e0674fa05f595584bf
Adam <adam.wilcz7@gmai.com>
2022-08-20 13:09:20 +0200

    Created debugging interface for reading files

--- countries_characteristics.py
+++ countries_characteristics.py
@@ -1,1 +1,51 @@
- print(
+import os
+
+import pandas as pd
+
+def binary_search():
+    pass
+
+# Countries data files have number index from 0, tehn columns: country, score, rank
+
+class CountriesData:
+    
+    
+    def __init__(self):
+        self.country_key = "country"
+
+        self.countries_df = self.read_data("country_code_capital_location_continent.parquet")
+        
+        self.happiness = self.read_data("happiness_2020.parquet")
+        
+        self.countries_different_data = [
+            self.happiness,
+        ]
+    
+    def read_data(self, filename: str) -> pd.DataFrame:
+        return pd.read_parquet(filename, header=0)
+    
+    def list_countries_data_file_names(self) -> [pd.DataFrame]:
+        return [f for f in os.listdir() if f.endswith(".parquet") and f != self.countries]
+    
+    def assert_key_present(self, df):
+        if self.country_key not in self.countries_df.columns:
+            raise KeyError(f"Key: {self.country_key} not present in countries file columns: {self.countries_df.columns}")
+        for df in self.countries_different_data:
+            if self.country_key not in df.columns:
+                raise KeyError(f"Key: {self.country_key} not present in countries data columns: {df.columns}")
+    
+    def country_name_mismatch(self):
+        self.assert_key_present()
+        
+        countries = self.countries_df["country"].tolist()
+        
+        for df in self.countries_different_data:
+            for country_name in df["country"].tolist():
+                if country_name not in countries:
+                    raise KeyError(f"{country_name} not present in countries file")
+            
+            
+        
+files = CountriesData()
+print(files.countries)
+print(files.countries_data)
